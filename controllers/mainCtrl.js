500c9ae422b95ded1603a8744038d73d575b4c51
Manon BERGUIG <mberguig@boursorama.fr>
2017-08-03 13:59:38 +0200

    maj

--- controllers/mainCtrl.js
+++ controllers/mainCtrl.js
@@ -4,29 +4,120 @@ var app = angular.module('myApp', ['services', 'ngResource']);
 
 app.controller('mainCtrl', ['$scope', '$resource', 'GetData', function($scope, $resource, GetData){
 $scope.date = new Date();
-
+$scope.isCTI = false;
 //--------------------------------------------
 //Initialize and load batchs data
 //--------------------------------------------
 $scope.init = function(){
   $scope.batchs = {};
+  $scope.batchsHistorical = [];
   $scope.batchSelected = {};
   $scope.stepVisible = false;
   $scope.UTVisible = false;
   $scope.batchsLoaded = false;
-  getBatchs();
+  $scope.page = 0;
+  $scope.nbTotalPages = 1;
+  $scope.status = "ALL";
+  $scope.nbTotalBatchs = {
+    ALL: "...",
+    OK: "...",
+    KOF:"...",
+    KOT:"...",
+    EC:"..."
+  };
+  var formatedDate = $scope.date.toISOString().substring(0,10);
+  GetData.getNbResultsEC(formatedDate, $scope.isCTI).then(function(response){
+    $scope.nbTotalBatchs.EC = response.result;
+    console.log("EC : ", response);
+  });
+  GetData.getNbResultsOK("OK", formatedDate, $scope.isCTI).then(function(response){
+    $scope.nbTotalBatchs.OK = response.result;
+    console.log("OK : ", response);
+  });
+  GetData.getNbResultsKOF("KOF", formatedDate, $scope.isCTI).then(function(response){
+    $scope.nbTotalBatchs.KOF = response.result;
+    console.log("KOF : ", response);
+  });
+  GetData.getNbResultsKOT("KOT", formatedDate, $scope.isCTI).then(function(response){
+    $scope.nbTotalBatchs.KOT = response.result;
+    console.log("KOT : ", response);
+  });
+  $(".btnStatus").removeClass("selected");
+  $("#btnAll").addClass("selected");
+  getBatchs(formatedDate);
 }
 
 //--------------------------------------------
 //Load batchs data
 //--------------------------------------------
-var getBatchs = function(){
-  var formatedDate = $scope.date.toISOString().substring(0,10);
-  GetData.getBatchs(formatedDate).then(function(response){
+var getBatchs = function(formatedDate){
+  GetData.getBatchs(formatedDate, $scope.page, $scope.isCTI).then(function(response){
     console.log(response);
     $scope.batchs = response;
+      console.log($scope.batchs);
     $scope.batchsLoaded = true;
   });
+    var formatedDate = $scope.date.toISOString().substring(0,10);
+  GetData.getNbResults(formatedDate, $scope.isCTI).then(function(response){
+    console.log(response);
+    $scope.nbTotalBatchs.ALL = response.result;
+    $scope.nbTotalPages = Math.ceil($scope.nbTotalBatchs.ALL /10);
+    console.log("  $scope.nbTotalPages : ",   $scope.nbTotalPages);
+  });
+
+}
+
+//----------PAGINATION ----------- //
+$scope.getMoreBatchs = function(){
+  console.log("isCTI : ", $scope.isCTI);
+  if($scope.batchsHistorical[$scope.page] == undefined){
+      $scope.batchsHistorical[$scope.page]=$scope.batchs;
+  }
+  $scope.page +=1;
+  if($scope.batchsHistorical[$scope.page] == undefined) { //si l'historique n'existe pas pour la page demandée
+    var formatedDate = $scope.date.toISOString().substring(0,10);
+    switch($scope.status){
+      case "ALL":
+        GetData.getBatchs(formatedDate, $scope.page, $scope.isCTI).then(function(response){
+          console.log(response);
+          $scope.batchs = response;
+          $scope.batchsLoaded = true;
+        });
+      break;
+      case "OK":
+        GetData.getBatchsOK(formatedDate, $scope.page, $scope.isCTI).then(function(response){
+          console.log(response);
+          $scope.batchs = response;
+          $scope.batchsLoaded = true;
+        });
+      break;
+      case "KOF":
+        GetData.getBatchsKOF(formatedDate, $scope.page, $scope.isCTI).then(function(response){
+          console.log(response);
+          $scope.batchs = response;
+          $scope.batchsLoaded = true;
+        });
+      break;
+      case "KOT":
+        GetData.getBatchsKOT(formatedDate, $scope.page, $scope.isCTI).then(function(response){
+          console.log(response);
+          $scope.batchs = response;
+          $scope.batchsLoaded = true;
+        });
+      break;
+    }
+
+  }else {
+    $scope.batchs = $scope.batchsHistorical[$scope.page];
+  }
+}
+
+$scope.getLessBatchs = function(){
+  if($scope.batchsHistorical[$scope.page] == undefined){
+    $scope.batchsHistorical[$scope.page]=$scope.batchs;
+  }
+  $scope.page -=1;
+  $scope.batchs = $scope.batchsHistorical[$scope.page];
 }
 
 //--------------------------------------------
@@ -42,53 +133,113 @@ $scope.batchClicked = function(batch){
     $scope.batchSelected = batch;
   //  selectDetailsToShow(batchId);
     // TODO : REQUETE AVEC BATCHID
+    $scope.getUnits(batch.treatmentId);
   }else {
     if(($scope.batchSelected == null)||($scope.batchSelected.unitId != batch.unitId)){
       $scope.batchSelected = batch;
     //  selectDetailsToShow(batchId);
       // TODO : REQUETE AVEC BATCHID
+      $scope.getUnits(batch.treatmentId);
     }else {
       $scope.UTVisible = false;
     }
   }
 }
 
+$scope.CTIClicked = function(){
+  $scope.CTI = !$scope.CTI;
+  if($scope.status =="ALL") {
+      $scope.init();
+    }else {
+    $scope.getBatchByStatus($scope.status, "DIFFSTATUS");
+    }
+
+  }
+
+
 //--------------------------------------------
 //Get steps for the Unit clicked
 //--------------------------------------------
 $scope.getSteps = function(unitId){
   console.log("unitId : ", unitId);
-if($scope.batchs[$scope.batchSelected.id].treatmentUnits[unitId].steps[0] == null){
+  console.log("$scope.batchs : ", $scope.batchs);
+  console.log("$scope.batchSelected : ", $scope.batchSelected);
+if($scope.batchs[$scope.batchSelected.treatmentId].treatmentUnits[unitId].steps[0] == null){
     GetData.getSteps(unitId).then(function(response){
-      $scope.batchs[$scope.batchSelected.id].treatmentUnits[unitId].steps = response;
-      $scope.batchs[$scope.batchSelected.id].treatmentUnits[unitId].steps.loaded = true;
-      $scope.batchSelected = $scope.batchs[$scope.batchSelected.id];
+      $scope.batchs[$scope.batchSelected.treatmentId].treatmentUnits[unitId].steps = response;
+      $scope.batchs[$scope.batchSelected.treatmentId].treatmentUnits[unitId].steps.loaded = true;
+      $scope.batchSelected = $scope.batchs[$scope.batchSelected.treatmentId];
+      console.log("steps : ", response);
     });
   }
 }
 
 //--------------------------------------------
-//Filter functionning
+//Get units for the Unit clicked
 //--------------------------------------------
-$scope.addFilter = function(fitler){
-  switch (filter) {
-    case OK:
-    //  $scope.filters.push("OK");
-    break;
-    case KOF:
+$scope.getUnits = function(batchId){
 
-    break;
-    case KOT:
-
-    break;
-    case ALL:
-
-    break;
-    default:
-
+if($scope.batchs[$scope.batchSelected.treatmentId].treatmentUnits[0] == null){
+    GetData.getUnits(batchId).then(function(response){
+      $scope.batchs[$scope.batchSelected.treatmentId].treatmentUnits = response;
+      $scope.batchs[$scope.batchSelected.treatmentId].treatmentUnits.loaded = true;
+      $scope.batchSelected = $scope.batchs[$scope.batchSelected.treatmentId];
+      console.log("units : ", response);
+    });
   }
 }
 
+
+
+//--------------------------------------------
+//Filter functionning
+//--------------------------------------------
+$scope.getBatchByStatus = function(status, page){
+  if(status != $scope.status){
+    $scope.batchs = {};
+    $scope.batchsHistorical = [];
+    $scope.batchSelected = {};
+    $scope.stepVisible = false;
+    $scope.UTVisible = false;
+    $scope.batchsLoaded = false;
+    $scope.page = page;
+    $(".btnStatus").removeClass("selected");
+    console.log("filter : ", status);
+    var formatedDate = $scope.date.toISOString().substring(0,10);
+    switch (status) {
+      case "OK":
+          GetData.getBatchsOK(formatedDate, $scope.page, $scope.isCTI).then(function(response){
+            console.log(response);
+            $scope.batchs = response;
+            $scope.batchsLoaded = true;
+          });
+          $("#btnOK").addClass("selected");
+      break;
+      case "KOF":
+          GetData.getBatchsKOF(formatedDate, $scope.page, $scope.isCTI).then(function(response){
+            console.log(response);
+            $scope.batchs = response;
+            $scope.batchsLoaded = true;
+          });
+          $("#btnKOF").addClass("selected");
+      break;
+      case "KOT":
+        GetData.getBatchsKOT(formatedDate, $scope.page, $scope.isCTI).then(function(response){
+          console.log(response);
+          $scope.batchs = response;
+          $scope.batchsLoaded = true;
+        });
+        $("#btnKOT").addClass("selected");
+      break;
+      case "EC":
+
+      break;
+      default:
+
+      }
+      $scope.status = status;
+  }
+}
 
   }]);
 
--- services/service.js
+++ services/service.js
@@ -2,18 +2,80 @@ var services = angular.module('services', ['ngResource']);
 
 services.factory('GetData', function($resource){
 
-  var Batchs = $resource('http://localhost:8080/most/api/batchs/:date', {});
+  var Batchs = $resource('http://localhost:8080/most/api/batchs2/:date/:page/:isCTI', {});
+  var BatchsKOT = $resource('http://localhost:8080/most/api/batchsByStatus/KOT/:date/:page/:isCTI', {});
+  var BatchsKOF = $resource('http://localhost:8080/most/api/batchsByStatus/KOF/:date/:page/:isCTI', {});
+  var BatchsOK = $resource('http://localhost:8080/most/api/batchsByStatus/OK/:date/:page/:isCTI', {});
+  var Units = $resource('http://localhost:8080/most/api/units/:batchId', {});
   var Steps = $resource('http://localhost:8080/most/api/steps/:unitId', {});
+  var nbResults = $resource('http://localhost:8080/most/api/getTotalNumberOfBatchs/:date/:isCTI', {});
+  var nbResultsOK = $resource('http://localhost:8080/most/api/getTotalNumberOfBatchsByStatus/:status/:date/:isCTI', {});
+  var nbResultsKOF = $resource('http://localhost:8080/most/api/getTotalNumberOfBatchsByStatus/:status/:date/:isCTI', {});
+  var nbResultsKOT = $resource('http://localhost:8080/most/api/getTotalNumberOfBatchsByStatus/:status/:date/:isCTI', {});
+  var nbResultsEC = $resource('http://localhost:8080/most/api/getTotalNumberOfBatchsEC/:date/:isCTI', {});
   return {
-    getBatchs : function(formatedDate){
-                  var query = Batchs.get({date:formatedDate}).$promise.then(function(response){
-                              return response;
-                          });
-                return query;
-              },
-
+    getBatchs : function(formatedDate, page, isCTI){
+                    var query = Batchs.get({date:formatedDate, page:page, isCTI:isCTI}).$promise.then(function(response){
+                    return response;
+                    });
+                    return query;
+                  },
+    getBatchsKOT : function(formatedDate, page, isCTI){
+                  var query = BatchsKOT.get({date:formatedDate, page:page, isCTI:isCTI}).$promise.then(function(response){
+                    return response;
+                  });
+                  return query;
+                  },
+    getBatchsKOF : function(formatedDate, page, isCTI){
+                  var query = BatchsKOF.get({date:formatedDate, page:page, isCTI:isCTI}).$promise.then(function(response){
+                    return response;
+                  });
+                  return query;
+                  },
+    getBatchsOK : function(formatedDate, page, isCTI){
+                  var query = BatchsOK.get({date:formatedDate, page:page, isCTI:isCTI}).$promise.then(function(response){
+                    return response;
+                  });
+                  return query;
+                  },
+    getUnits : function(batchId){
+                  var query = Units.get({batchId:batchId}).$promise.then(function(response){
+                    return response;
+                  });
+                  return query;
+                  },
     getSteps : function(unitId){
                   var query = Steps.get({unitId:unitId}).$promise.then(function(response){
+                                return response;
+                            });
+                  return query;
+                },
+    getNbResults : function(formatedDate, isCTI){
+                  var query = nbResults.get({date:formatedDate, isCTI:isCTI}).$promise.then(function(response){
+                                return response;
+                            });
+                  return query;
+                },
+    getNbResultsEC : function(formatedDate, isCTI){
+                  var query = nbResultsEC.get({date:formatedDate, isCTI:isCTI}).$promise.then(function(response){
+                                return response;
+                            });
+                    return query;
+                },
+    getNbResultsOK : function(status, formatedDate, isCTI){
+                  var query = nbResultsOK.get({status:status,date:formatedDate, isCTI:isCTI}).$promise.then(function(response){
+                                return response;
+                            });
+                  return query;
+                },
+    getNbResultsKOF : function(status, formatedDate, isCTI){
+                  var query = nbResultsKOF.get({status:status,date:formatedDate, isCTI:isCTI}).$promise.then(function(response){
+                                return response;
+                            });
+                  return query;
+                },
+    getNbResultsKOT : function(status, formatedDate, isCTI){
+                  var query = nbResultsKOT.get({status:status,date:formatedDate, isCTI:isCTI}).$promise.then(function(response){
                                 return response;
                             });
                   return query;
